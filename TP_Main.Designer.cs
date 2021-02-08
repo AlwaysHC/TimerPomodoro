30919b697f252afe4452df1b5aa72acd630bc88c
Mirko Sacripanti <mirko@nextware.it>
2021-02-08 12:39:33 +0100

    First commit

---
+++ TP_Main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TimerPomodoro {
+    partial class TP_Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.TimerEnd = new System.Windows.Forms.Timer(this.components);
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.btnFerma = new System.Windows.Forms.Button();
+            this.lblAvvio = new System.Windows.Forms.Label();
+            this.TimerStop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.BackColor = System.Drawing.Color.Transparent;
+            this.lblTimer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTimer.Location = new System.Drawing.Point(0, 0);
+            this.lblTimer.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(202, 73);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "25:00";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblTimer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTimer_MouseMove);
+            // 
+            // TimerFine
+            // 
+            this.TimerEnd.Tick += new System.EventHandler(this.TimerEnd_Tick);
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Location = new System.Drawing.Point(0, 0);
+            this.btnAvvia.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(49, 20);
+            this.btnAvvia.TabIndex = 1;
+            this.btnAvvia.Text = "Start";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnFerma
+            // 
+            this.btnFerma.Location = new System.Drawing.Point(154, 0);
+            this.btnFerma.Margin = new System.Windows.Forms.Padding(0);
+            this.btnFerma.Name = "btnFerma";
+            this.btnFerma.Size = new System.Drawing.Size(49, 20);
+            this.btnFerma.TabIndex = 2;
+            this.btnFerma.Text = "Stop";
+            this.btnFerma.UseVisualStyleBackColor = true;
+            this.btnFerma.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblAvvio
+            // 
+            this.lblAvvio.Location = new System.Drawing.Point(51, 3);
+            this.lblAvvio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAvvio.Name = "lblAvvio";
+            this.lblAvvio.Size = new System.Drawing.Size(100, 19);
+            this.lblAvvio.TabIndex = 3;
+            this.lblAvvio.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblAvvio.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblStart_MouseMove);
+            // 
+            // TimerFermo
+            // 
+            this.TimerStop.Interval = 1000;
+            this.TimerStop.Tick += new System.EventHandler(this.TimerStop_Tick);
+            // 
+            // TP_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 73);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnFerma);
+            this.Controls.Add(this.btnAvvia);
+            this.Controls.Add(this.lblAvvio);
+            this.Controls.Add(this.lblTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TP_Main";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer Pomodoro";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer TimerEnd;
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.Button btnFerma;
+        private System.Windows.Forms.Label lblAvvio;
+        private System.Windows.Forms.Timer TimerStop;
+    }
+}
+
---
+++ TP_Main.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using TimerPomodoro.Properties;
+
+namespace TimerPomodoro {
+    public partial class TP_Main: Form {
+        public TP_Main() {
+            InitializeComponent();
+            Icon = Resources.Pomodoro;
+
+            StreamReader SR = null;
+            try {
+                SR = new StreamReader("TimerPomodoro.txt");
+                _PomodoriStart = Int32.Parse(SR.ReadLine());
+                _PomodoriToday = Int32.Parse(SR.ReadLine());
+                _PomodoriYesterday = Int32.Parse(SR.ReadLine());
+                _PomodoriDate = DateTime.Parse(SR.ReadLine());
+            }
+            catch {
+            }
+            finally {
+                if (SR != null) {
+                    SR.Close();
+                }
+            }
+            if (_PomodoriDate.Date != DateTime.Now.Date) {
+                _PomodoriYesterday = _PomodoriToday;
+                _PomodoriToday = 0;
+            }
+
+            btnStop_Click(null, null);
+        }
+
+        private DateTime _DateTimeStop = DateTime.MinValue;
+        private TimeSpan _TimeSpanStop = new TimeSpan(0, 0, 0);
+        private DateTime _TimeSpanLastStop = DateTime.MinValue;
+        private int _PomodoriStart = 0;
+        private int _PomodoriToday = 0;
+        private int _PomodoriYesterday = 0;
+        private DateTime _PomodoriDate = DateTime.MinValue;
+
+        private void btnStop_Click(object sender, EventArgs e) {
+            Graphics G = lblTimer.CreateGraphics();
+            SizeF S = G.MeasureString(" 25:00.0 ", lblTimer.Font);
+            lblTimer.Font = new Font(lblTimer.Font.FontFamily, lblTimer.Font.Size * (lblTimer.Width / S.Width), lblTimer.Font.Style);
+
+            TimerEnd.Enabled = false;
+            TimerStop.Enabled = true;
+            _DateTimeStop = DateTime.MinValue;
+            _TimeSpanStop = new TimeSpan(0, 0, 0);
+            _TimeSpanLastStop = DateTime.Now;
+            lblTimer.Text = new TimeSpan(0, 25, 0).ToString("mm\\:ss\\.f");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            _DateTimeStop = DateTime.Now.AddMinutes(25);
+            TimerEnd.Enabled = true;
+            TimerStop.Enabled = false;
+            lblAvvio.Text = DateTime.Now.ToString("HH\\:mm\\:ss") + " - " + _PomodoriToday + " (" + _PomodoriYesterday + ") " + _PomodoriStart;
+        }
+
+        private void TimerEnd_Tick(object sender, EventArgs e) {
+            DateTime Ora = DateTime.Now;
+            if (Ora >= _DateTimeStop) {
+                Rectangle R = Screen.GetWorkingArea(lblTimer);
+                Left = (R.Width - Width) / 2;
+                Top = (R.Height - Height) / 2;
+
+                _PomodoriToday++;
+                _PomodoriStart++;
+                Save();
+                btnStop_Click(null, null);
+
+                BackColor = Color.Yellow;
+                Application.DoEvents();
+                Thread.Sleep(500);
+                BackColor = Color.Blue;
+                Application.DoEvents();
+                Thread.Sleep(500);
+                BackColor = Color.Red;
+                Application.DoEvents();
+                Thread.Sleep(500);
+                BackColor = SystemColors.Control;
+                Application.DoEvents();
+            }
+            else {
+                lblTimer.Text = (_DateTimeStop.Subtract(Ora)).ToString("mm\\:ss\\.f");
+            }
+        }
+
+        private void lblTimer_MouseMove(object sender, MouseEventArgs e) {
+            MoveWindow();
+        }
+
+        private void MoveWindow() {
+            Point Pos = PointToClient(MousePosition);
+            int T = Top;
+            int L = Left;
+            int X4 = lblTimer.Width / 4;
+            int Y3 = lblTimer.Height / 3;
+            if (Pos.X < X4) {
+                if (Pos.Y < Y3) {
+                    T += 5;
+                    L += 5;
+                }
+                else {
+                    if (Pos.Y < Y3 * 2) {
+                        L += 5;
+                    }
+                    else {
+                        T -= 5;
+                        L += 5;
+                    }
+                }
+            }
+            else {
+                if (Pos.X < X4 * 3) {
+                    if (Pos.Y < Y3) {
+                        T += 5;
+                    }
+                    else {
+                        if (Pos.Y > Y3 * 2) {
+                            T -= 5;
+                        }
+                    }
+                }
+                else {
+                    if (Pos.Y < Y3) {
+                        T += 5;
+                        L -= 5;
+                    }
+                    else {
+                        if (Pos.Y < Y3 * 2) {
+                            L -= 5;
+                        }
+                        else {
+                            T -= 5;
+                            L -= 5;
+                        }
+                    }
+                }
+            }
+            Rectangle R = Screen.GetWorkingArea(lblTimer);
+            if (T <= (R.Top + 5)) {
+                T = R.Top + 5;
+            }
+            if (T + Height >= (R.Bottom - 5)) {
+                T = (R.Bottom - Height) - 5;
+            }
+            if (L <= (R.Left + 5)) {
+                L = R.Left + 5;
+            }
+            if (L + Width >= (R.Right - 5)) {
+                L = (R.Right - Width) - 5;
+            }
+            Top = T;
+            Left = L;
+        }
+
+        private void TimerStop_Tick(object sender, EventArgs e) {
+            DateTime Now = DateTime.Now;
+            if ((Now - _TimeSpanLastStop).TotalMilliseconds < TimerStop.Interval * 5) {
+                _TimeSpanStop = _TimeSpanStop.Add(Now - _TimeSpanLastStop);
+            }
+            if (_TimeSpanLastStop.DayOfYear != Now.DayOfYear) {
+                _PomodoriYesterday = _PomodoriToday;
+                _PomodoriToday = 0;
+                Save();
+            }
+            _TimeSpanLastStop = Now;
+            lblAvvio.Text = _TimeSpanStop.ToString("hh\\:mm\\:ss") + " - " + _PomodoriToday + " (" + _PomodoriYesterday + ") " + _PomodoriStart;
+        }
+
+        private void lblStart_MouseMove(object sender, MouseEventArgs e) {
+            MoveWindow();
+        }
+
+        private void Save() {
+            try {
+                StreamWriter SW = new StreamWriter("TimerPomodoro.txt", false);
+                SW.WriteLine(_PomodoriStart);
+                SW.WriteLine(_PomodoriToday);
+                SW.WriteLine(_PomodoriYesterday);
+                SW.WriteLine(DateTime.Now);
+                SW.Close();
+            }
+            catch {
+            }
+        }
+    }
+}
